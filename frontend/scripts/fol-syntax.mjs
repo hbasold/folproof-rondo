186b8f66e4a127e3c95f511b0bffe1f2e6707987
Jort van Leenen <jort@vleenen.nl>
2025-02-26 16:44:28 +0100

    Improve syntax highlighting
    It is now more efficient, works for | and - well and fixes and issue where quantifiers were not identified when surrounded with whitespace.

--- frontend/scripts/fol-syntax.mjs
+++ frontend/scripts/fol-syntax.mjs
@@ -36,33 +36,42 @@ const folSyntax = StreamLanguage.define({
   token(stream) {
     if (stream.eatSpace()) return null;
 
-    if (stream.match(/^#.*$/, true)) return "comment";
+    if (stream.match(/^#.*$/)) return "comment";
 
-    if (stream.peek() === ":") {
-      stream.next();
-      stream.eatWhile((ch) => ch !== "#");
-      return "string";
-    }
+    if (stream.match(/^:[^#]*/)) return "string";
 
     if (simpleOperators.has(stream.peek())) {
       stream.next();
       return "operator";
     }
 
-    if (quantifiers.has(stream.peek())) {
-      // A or E are only valid quantifiers if followed by a variable and dot
-      if (stream.match(/^[∀A∃E][a-zA-Z_][a-zA-Z_'"0-9|]*\./, false)) {
-        stream.next();
-        return "logicOperator";
-      }
+    // A or E are only valid quantifiers if followed by a variable and dot
+    if (
+      quantifiers.has(stream.peek()) &&
+      stream.match(/^[∀A∃E]\s*[a-zA-Z_][a-zA-Z_'"0-9|]*\s*\./, false)
+    ) {
+      stream.next();
+      return "logicOperator";
     }
 
-    const match = stream.match(/[a-z]*/);
+    const match = stream.match(/^[a-z]*/, false);
     if (match && operatorKeywords.has(match[0])) {
+      stream.eatWhile(/[a-z]/);
       return "operator";
     }
 
-    if (stream.match(/(->)|(<->)|(_\|_)/)) return "operator";
+    if (stream.match(/^((->)|(<->)|(_\|_))/)) return "operator";
+
+    // Ensures that | is not matched below when it is in a variable (see lexer)
+    if (stream.match(/^[a-zA-Z_][a-zA-Z_'"0-9|]*/)) return "variable";
+
+    // Ensures that - is not matched below when it is in numrange (see lexer)
+    if (stream.match(/^\d+(-\d+)?/)) return "number";
+
+    if (stream.peek() === "-" || stream.peek() === "|") {
+      stream.eatWhile((c) => c === "-" || c === "|");
+      return "namespace";
+    }
 
     // Default case, do nothing
     stream.next();
@@ -76,6 +85,7 @@ const folHighlightStyle = HighlightStyle.define([
   { tag: tags.string, color: "#DDAA33" },
   { tag: tags.operator, color: "#6699CC", fontWeight: "bold" },
   { tag: tags.logicOperator, color: "#EE3377", fontWeight: "bold" },
+  { tag: tags.namespace, color: "#44BB99", fontWeight: "bold" },
 ]);
 
 export function folLanguage() {
