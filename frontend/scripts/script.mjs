495427949f7f755a55ce780bab1d058b48b1fce4
Jort van Leenen <jort@vleenen.nl>
2025-02-26 22:12:20 +0100

    Fix scope service worker

--- frontend/scripts/script.mjs
+++ frontend/scripts/script.mjs
@@ -29,7 +29,7 @@ async function registerServiceWorker() {
   if ("serviceWorker" in navigator) {
     try {
       await navigator.serviceWorker.register("service-worker.js", {
-        // scope: "./",
+        scope: "./",
       });
     } catch (error) {
       console.error(`Service worker registration failed with: ${error}`);
