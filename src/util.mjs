ae306d8d18e74b762f324da6b63abe8bdc7d0555
Henning Basold <h.basold@liacs.leidenuniv.nl>
2025-03-03 12:50:17 +0100

    Disable colouring in debug messages to get pretty printing

--- src/util.mjs
+++ src/util.mjs
@@ -2,7 +2,8 @@ import { getDebugMode, noticeColour } from "./config.mjs";
 
 function debugMessage() {
   if (getDebugMode()) {
-    console.log(noticeColour(...Array.prototype.slice.call(arguments)));
+      // console.log(noticeColour(...Array.prototype.slice.call(arguments)));
+    console.log(Array.prototype.slice.call(arguments));
   }
 }
 
